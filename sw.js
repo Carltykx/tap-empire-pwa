cccf42905f298fc33231af7fc3df3a59a6a6ba67
Carltykx <carltykx@gmail.com>
2025-10-24 16:48:01 +0300

    Add service worker for PWA asset caching
    Implement a service worker for caching assets.

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'tap-empire-v1';
+const BASE_PATH = '/tap-empire-pwa';
+const urlsToCache = [
+    BASE_PATH + '/',
+    BASE_PATH + '/index.html',
+    BASE_PATH + '/login.html',
+    BASE_PATH + '/manifest.json',
+    BASE_PATH + '/icon-192.png',
+    BASE_PATH + '/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
